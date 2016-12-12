4ee1f8c14856534060c8ed2683cc02e9c4b5e13c
liuyakun <happy_lucky52@163.com>
2016-12-12 18:39:32 +0800

    取非极端元素

---
+++ introduction/unextreme/main.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <vector>
+#include "unextreme.h"
+
+int main(int argc,char **argv)
+{
+	std::vector<int> iVec;
+	for(int i = 0; i < 20; ++i)
+		iVec.push_back(i);
+
+	UnExtreme<int> uEx(iVec);
+	std::cout << "The unextreme element is : " << uEx.UnExtremeElement() << std::endl;
+
+	return 0;
+}
---
+++ introduction/unextreme/unextreme_impl.h
@@ -0,0 +1,51 @@
+#ifndef _UNEXTREME_IMPL_H_
+#define _UNEXTREME_IMPL_H_
+
+#include <cstdlib>
+#include <vector>
+
+template <typename T>
+T UnExtreme::Max(T a,T b,T c)
+{
+	return Max2(Max2(a,b),Max2(b,c));
+}
+
+template <typename T>
+T UnExtreme::Min(T a,T b,T c)
+{
+	return Min(Min(a,b),Min(b,c));
+}
+
+pIndex UnExtreme::RandProductThreeIndex()
+{
+	pIndex pIdx = new  Index;
+	int vec_size = vec_.size();
+	
+	srand(std::time(0));
+	pIdx -> idx1 = rand() % vec_size;
+	pIdx -> idx2 = rand() % vec_size;
+	pIdx -> idx3 = rand() % vec_size;
+
+	return pIdx;
+}
+
+template <typename T>
+T UnExtreme::UnExtremeElement()
+{
+	pIndex p = RandProductThreeIndex();
+
+	T min = Min(vec_(p -> idx1),vec_(p -> idx2),vec_(p -> idx3));
+	T max = Max(vec_(p -> idx1),vec_(p -> idx2),vec_(p -> idx3));
+
+	T result = min;
+	if(vec_(p -> idx1) != min && vec_(p -> idx1) != max)
+		result = vec_(p -> idx1);
+	if(vec_(p -> idx2) != min && vec_(p -> idx2) != max)
+		result = vec_(p -> idx2);
+	if(vec_(p -> idx3) != min && vec_(p -> idx3) != max)
+		result = vec_(p -> idx3);
+	
+	return result;
+}
+
+#endif
