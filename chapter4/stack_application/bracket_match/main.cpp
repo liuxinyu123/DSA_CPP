37d960e8fd960b94dd0a74aa2f81f2a9d7826434
liuyakun <happy_lucky52@163.com>
2017-03-10 20:56:14 +0800

    bracket_match

---
+++ chapter4/stack_application/bracket_match/main.cpp
@@ -0,0 +1,80 @@
+#include <iostream>
+#include <string>
+#include <vector>
+#include "stack.h"
+
+using std::cout;
+using std::endl;
+using std::string;
+using std::vector;
+
+void test_recursion (vector<string> &strVec);
+void test_iteration (vector<string> &strVec);
+bool bracket_parse_recursion (const string &s, int lo, int hi);
+bool bracket_parse_iteration (const string &s, int lo, int hi);
+
+void trim (const string &s, int &lo, int &hi);//	确定两边都是括号，如果没有lo > hi
+int divide (const string &s, int lo, int hi);
+
+int main (int argc, char *argv[])
+{
+	vector<string> strVec;
+	strVec.push_back ("(a+b)*c+(d+e)");
+	strVec.push_back ("(a+b)*c+(d+e))");
+	strVec.push_back ("(a+b)*c+(d+e)/(s+w)");
+	strVec.push_back ("((a+b))*c+(()())d+e)");
+
+	test_recursion (strVec);
+	
+	return 0;	
+}
+
+void test_recursion (vector<string> &strVec)
+{
+	for (const auto &e : strVec)
+	{
+		if (bracket_parse_recursion (e, 0, e.size ()))
+			cout << "match ok" << endl;
+		else
+			cout << "not match" << endl;
+	}
+}
+
+void trim (const string &s, int &lo, int &hi)//	确定两边都是括号，如果没有lo > hi
+{
+	while ((lo <= hi) && (s[lo] != '(') && (s[lo] != ')'))
+		++lo;
+	while ((lo <= hi) && (s[hi] != '(') && (s[hi] != ')'))
+		--hi;
+}
+
+int divide (const string &s, int lo, int hi)//	在两端都是括号的式子中，找出中间切分点
+{
+	int mid = lo;
+	int crc = 1;//左右括号的差值
+	while ((0 != crc) && (mid++ <= hi))
+	{
+		if (s[mid] == ')')
+			--crc;
+		if (s[mid] == '(')
+			++crc;
+	}
+
+	return mid;
+}
+
+bool bracket_parse_recursion (const string &s, int lo, int hi)
+{
+	trim (s, lo, hi);
+	if (lo > hi)//表示没有括号
+		return true;
+	if ((s[lo] != '(') || (s[hi] != ')'))
+		return false;
+	int mid = divide (s, lo, hi);
+
+	if (mid > hi)//括号不平衡
+		return false;
+	return bracket_parse_recursion (s, lo+1, mid - 1) && bracket_parse_recursion (s, mid + 1, hi - 1);
+
+}
+
