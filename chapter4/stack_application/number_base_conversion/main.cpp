46a9a7f5e29d649ab7bd7f04086580ce3fb328d1
liuyakun <happy_lucky52@163.com>
2017-03-10 19:56:57 +0800

    number_base_conversion

--- chapter4/stack_application/number_base_conversion/main.cpp
+++ chapter4/stack_application/number_base_conversion/main.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <vector>
+#include <ctime>
 #include "stack.h"
 
 using std::cout;
@@ -8,16 +9,29 @@ using std::vector;
 
 void test_number_base_conversion_recursion (Stack<char> &s, vector<unsigned> v, unsigned base);
 void conversion_recursion (Stack<char> &s, unsigned number, unsigned base);
+void test_number_base_conversion_iteration (Stack<char> &s, vector<unsigned> v, unsigned base);
+void conversion_iteration (Stack<char> &s, unsigned number, unsigned base);
 
 int main (int argc, char *argv[])
 {
 	Stack<char> cSta;
 	vector<unsigned> iVec;
-	iVec.push_back (224);
-	iVec.push_back (45);
-	iVec.push_back (12);
-	iVec.push_back (23);
+
+	for (unsigned i = 0; i < 100; i++)
+		iVec.push_back (i);
+
+	cout << "test_number_base_conversion_recursion:" << endl;
+	std::clock_t b1 = clock ();
 	test_number_base_conversion_recursion (cSta, iVec, 2);
+	std::clock_t e1 = clock ();
+	cout << "Time elapse: " << e1 - b1 << endl;
+
+	cout << "test_number_base_conversion_iteration: " << endl;
+	std::clock_t b2 = clock ();
+	test_number_base_conversion_iteration (cSta, iVec, 2);
+	std::clock_t e2 = clock ();
+	cout << "Time elapse: " << e2 - b2 << endl;
+
 
 	return 0;
 }
@@ -47,3 +61,26 @@ void conversion_recursion (Stack<char> &s, unsigned number, unsigned base)
 }
 
 
+void test_number_base_conversion_iteration (Stack<char> &s, vector<unsigned> v, unsigned base)
+{
+	for (const unsigned e : v)
+	{
+		cout << e << " = ";
+		conversion_recursion (s, e, base);
+		cout << endl;
+	}
+	
+}
+void conversion_iteration (Stack<char> &s, unsigned number, unsigned base)
+{
+	static char digit[] = {'0','1','2','3','4','5','6','7','8','9',
+			'A','B','C','D','F'};
+	while (0 < number)
+	{
+		s.push (digit[number % base]);
+		number /= base;
+	}	
+
+	while (!s.isEmpty ())
+		cout << s.pop ();
+}
