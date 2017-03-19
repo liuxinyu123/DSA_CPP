5b6c550a8cf333c4d39d0b6a65b2600c0d8ead72
liuyakun <happy_lucky52@163.com>
2017-03-19 22:02:27 +0800

    longest_common_subsquence

--- introduction/longest_common_subsquence/main.cpp
+++ introduction/longest_common_subsquence/main.cpp
@@ -9,11 +9,12 @@ int lcs (char *s1, int sz1, char *s2, int sz2);
 
 int main (int argc, char *argv[])
 {
-	char s1[] = "educational";
-	char s2[] = "advantage";
+	char s1[] = {'e', 'd', 'u', 'c', 'a', 't', 'i', 'o', 'n', 'a', 'l'};
+	char s2[] = {'a', 'd', 'v', 'a', 'n', 't', 'a', 'g', 'e'};
 	int sz1 = sizeof (s1) / sizeof (char);
 	int sz2 = sizeof (s2) / sizeof (char);
 
+	cout << sz1 << endl;
 	cout << lcs (s1, sz1, s2, sz2) << endl;
 
 	return 0;
