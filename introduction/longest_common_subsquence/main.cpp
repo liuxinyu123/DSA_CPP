dc0e45b5ddd8e02a7f10c26d3d70f401941df230
liuyakun <happy_lucky52@163.com>
2017-03-20 20:25:43 +0800

    longest_common_subsquence

--- introduction/longest_common_subsquence/main.cpp
+++ introduction/longest_common_subsquence/main.cpp
@@ -1,11 +1,15 @@
 #include <iostream>
+#include <cstring>
+
 
 #define max(a, b) ((a) > (b) ? (a) : (b))
+#define min(a, b) ((a) < (b) ? (a) : (b))
 
 using std::cout;
 using std::endl;
 
-int lcs (char *s1, int sz1, char *s2, int sz2);
+int lcs (char *s1, int sz1, char *s2, int sz2);//采用递归的方式求解
+int lcs_dp (char *s1, int sz1, char *s2, int sz2);//采用迭代的方式求解
 
 int main (int argc, char *argv[])
 {
@@ -16,6 +20,7 @@ int main (int argc, char *argv[])
 
 	//cout << sz1 << endl;
 	cout << lcs (s1, sz1, s2, sz2) << endl;
+	cout << lcs_dp (s1, sz1, s2, sz2) << endl;
 
 	return 0;
 }
@@ -39,4 +44,46 @@ int lcs (char *s1, int sz1, char *s2, int sz2)
 	
 }
 
+int lcs_dp (char *s1, int sz1, char *s2, int sz2)
+{
+	int **m;//需要维护一个矩阵
+	m = new int* [sz1];
 
+	for (int i = 0; i < sz1; ++i)
+		m[i] = new int[sz2];
+	std::memset (m, 0, sz1 * sz2);
+	int n = min (sz1, sz2);
+
+	for (int j = 0; j < n; ++j)
+	{
+		if (s1[j] == s2[j])//对角线初始化
+			m[j][j] = m[j - 1][j - 1] + 1;
+		else
+			m[j][j] = max (m[j - 1][j], m[j][j - 1]);
+
+		for (int i = j + 1; i < sz1; ++i)//初始化一列
+		{
+			if (s1[i] == s2[j])
+				m[i][j] = m[i - 1][j] + 1;
+			else
+				m[i][j] = m[i - 1][j];
+		}
+		for (int i = j + 1; i < sz2; ++i)//初始化一行
+		{
+			if (s1[j] == s2[i])
+				m[j][i] = m[j][i - 1] + 1;
+			else
+				m[j][i] = m[j][i - 1];
+		}
+
+	
+	}
+
+	//还需要考虑sz1和sz2 多余出的部分
+
+	int ret = m[n - 1][n - 1];
+	delete [] m;
+
+	return ret;
+
+}
